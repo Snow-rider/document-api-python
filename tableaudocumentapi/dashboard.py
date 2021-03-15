12cbed7ef6c03a05119e0eb19893d012e7a7684b
cedric deymier <cedric.deymier@wartsila.com>
2021-03-15 17:19:52 +0100

    Dashboard parsing tested on one workbook

--- tableaudocumentapi/dashboard.py
+++ tableaudocumentapi/dashboard.py
@@ -11,102 +11,103 @@ class Dashboard(object):
 
         self._dashboard_name = self._worksheetXmlElement.get('name')
 
-        self._datasources = self._worksheetViewXmlElement.find('./datasources')
+        self._datasources = self._worksheetXmlElement.find('./datasources')
         self._datasource_dependencies = list(
-            map(DatasourceDependency, self._worksheetViewXmlElement.findall('./datasource-dependencies')))
+            map(DatasourceDependency, self._worksheetXmlElement.findall('./datasource-dependencies')))
 
         self._dependent_on_datasources = self.get_names_of_dependency_datasources()  # list of names
         self._datasources_dependent_on_columns = self.get_names_of_columns_per_datasource()
-        self._zones = self._worksheetXmlElement.find('./zone')
+        self._zones = self._worksheetXmlElement.find('./zones')
 
 
 
-@property
-def dashboard_name(self):
-    return self._dashboard_name
+    @property
+    def dashboard_name(self):
+        return self._dashboard_name
 
 
-def get_names_of_dependency_datasources(self):
-    sub_datasources = self._datasources.findall('./datasource')
-    datasource_names = list(dsxml.get('name') for dsxml in sub_datasources)
-    return datasource_names
+    def get_names_of_dependency_datasources(self):
+        sub_datasources = self._datasources.findall('./datasource') if \
+            self._datasources is not None else []
+        datasource_names = list(dsxml.get('name') for dsxml in sub_datasources)
+        return datasource_names
 
 
-def get_names_of_columns_per_datasource(self):
-    names_per_ds = {}
+    def get_names_of_columns_per_datasource(self):
+        names_per_ds = {}
 
-    # loop through the list of the names of the datasources
-    for ds in self._dependent_on_datasources:
-        # check against dependent columns and create a map (dictionary)
-        for ds_dep in self._datasource_dependencies:
-            if ds_dep.dependency_datasource_name == ds:
-                # column name is enough as it seems that the columns mirror column instances in this case
-                names_per_ds[ds] = list(cl.column_name for cl in ds_dep.columns)
-    return names_per_ds
-
-
-@property
-def dependent_on_datasources(self):
-    """List of data source names on which the worksheet is dependent.
-        :rtype: list()
-    """
-    return self._dependent_on_datasources
+        # loop through the list of the names of the datasources
+        for ds in self._dependent_on_datasources:
+            # check against dependent columns and create a map (dictionary)
+            for ds_dep in self._datasource_dependencies:
+                if ds_dep.dependency_datasource_name == ds:
+                    # column name is enough as it seems that the columns mirror column instances in this case
+                    names_per_ds[ds] = list(cl.column_name for cl in ds_dep.columns)
+        return names_per_ds
 
 
-@property
-def datasources_dependent_on_columns(self, ):
-    """Dictionary of data source names on which the worksheet is dependent together with columns of the data sources.
-       keys: data source names.
-       values: list of column names
-    """
-    return self._datasources_dependent_on_columns
+    @property
+    def dependent_on_datasources(self):
+        """List of data source names on which the worksheet is dependent.
+            :rtype: list()
+        """
+        return self._dependent_on_datasources
 
 
-@property
-def datasources(self):
-    return self._datasources
+    @property
+    def datasources_dependent_on_columns(self, ):
+        """Dictionary of data source names on which the worksheet is dependent together with columns of the data sources.
+           keys: data source names.
+           values: list of column names
+        """
+        return self._datasources_dependent_on_columns
 
 
-@property
-def datasources_dependencies(self):
-    return self._datasource_dependencies
+    @property
+    def datasources(self):
+        return self._datasources
 
 
-@property
-def zones(self):
-    return self._zones
-
-@zones.setter
-def zones(self,value):
-    self._zones = value
+    @property
+    def datasources_dependencies(self):
+        return self._datasource_dependencies
 
 
-# def modify_zone_param(self,datasource_name, field_names_to_be_changed_dict):
-#
-#     zone_with_param = self._zones.findall('./zone//*zone[@param]')
-#
-#     for elt in zone_with_param:
-#         if elt.get('param') in datasource_name:
-#             for field in field_names_to_be_changed_dict.keys:
-#                 if field in elt.get('param'):
-#                     new_param = field_names_to_be_changed_dict[field]
-#                     elt.set('param',new_param)
+    @property
+    def zones(self):
+        return self._zones
 
-def modify_zones(self,datasource_name, field_names_to_be_changed_dict):
+    @zones.setter
+    def zones(self,value):
+        self._zones = value
 
-    zone = self.zones
 
-    while zone:
-        if zone.get('param'):
-            if datasource_name in zone.get('param'):
-                for field in field_names_to_be_changed_dict.keys:
-                    if field in zone.get('param'):
-                        new_param = field_names_to_be_changed_dict[field]
-                        zone.set('param',new_param)
+    # def modify_zone_param(self,datasource_name, field_names_to_be_changed_dict):
+    #
+    #     zone_with_param = self._zones.findall('./zone//*zone[@param]')
+    #
+    #     for elt in zone_with_param:
+    #         if elt.get('param') in datasource_name:
+    #             for field in field_names_to_be_changed_dict.keys:
+    #                 if field in elt.get('param'):
+    #                     new_param = field_names_to_be_changed_dict[field]
+    #                     elt.set('param',new_param)
 
-        zone = zone.find('zone')
+    def modify_zones(self,datasource_name, field_names_to_be_changed_dict):
 
-    return zone
+        zone = self.zones.find('./zone')
+
+        while zone:
+            if zone.get('param'):
+                if datasource_name in zone.get('param'):
+                    for field in field_names_to_be_changed_dict.keys:
+                        if field in zone.get('param'):
+                            new_param = field_names_to_be_changed_dict[field]
+                            zone.set('param',new_param)
+
+            zone = zone.find('./zone')
+
+        return zone
 
 
 
