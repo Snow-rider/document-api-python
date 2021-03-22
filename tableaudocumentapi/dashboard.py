abcabf8a24e1f20df2c58cebd48230ccf8e04127
Lucie Hromadkova <lucie.hromadkova@eniram.fi>
2021-03-22 22:59:47 +0200

    fixed the doc stringsand variable names for Dashboard and ZoneParam so they correspond to the elements, removed unused import, unused setter and unused function

--- tableaudocumentapi/dashboard.py
+++ tableaudocumentapi/dashboard.py
@@ -1,12 +1,11 @@
-from tableaudocumentapi.worksheet_subelements import LayoutOptions, WorksheetPane, WorksheetStyleRule, WorksheetRowsOrCols, JoinLodExcludeOverrides
 from tableaudocumentapi.datasource_dependency import DatasourceDependency
 
 
 class ZoneParam(object):
-    """A class representing worksheet object."""
+    """A class representing zone object."""
 
     def __init__(self, zoneparamXmlElement):
-        """Constructor for XMl element representing Tableau worksheet with its children XML elements."""
+        """Constructor for XMl element representing zone object in dashboard XML element."""
 
         self._zoneparamXmlElement = zoneparamXmlElement
         self._param = self._zoneparamXmlElement.get('param')
@@ -27,22 +26,22 @@ class ZoneParam(object):
 
 
 class Dashboard(object):
-    """A class representing worksheet object."""
+    """A class representing dashboard object."""
 
-    def __init__(self, worksheetXmlElement):
-        """Constructor for XMl element representing Tableau worksheet with its children XML elements."""
+    def __init__(self, dashboardxmlelement):
+        """Constructor for XMl element representing dashboard XML (in workbook root) with its children XML elements."""
 
-        self._worksheetXmlElement = worksheetXmlElement
+        self._dashboardxmlelement = dashboardxmlelement
 
-        self._dashboard_name = self._worksheetXmlElement.get('name')
+        self._dashboard_name = self._dashboardxmlelement.get('name')
 
-        self._datasources = self._worksheetXmlElement.find('./datasources')
+        self._datasources = self._dashboardxmlelement.find('./datasources')
         self._datasource_dependencies = list(
-            map(DatasourceDependency, self._worksheetXmlElement.findall('./datasource-dependencies')))
+            map(DatasourceDependency, self._dashboardxmlelement.findall('./datasource-dependencies')))
 
         self._dependent_on_datasources = self.get_names_of_dependency_datasources()  # list of names
         self._datasources_dependent_on_columns = self.get_names_of_columns_per_datasource()
-        self._zones_xml = self._worksheetXmlElement.findall('./zones//zone[@param]')
+        self._zones_xml = self._dashboardxmlelement.findall('./zones//zone[@param]')
         self._zones_with_param = list(map(ZoneParam, self._zones_xml)) if self._zones_xml else []
 
 
@@ -51,13 +50,11 @@ class Dashboard(object):
     def dashboard_name(self):
         return self._dashboard_name
 
-
     def get_names_of_dependency_datasources(self):
         sub_datasources = self._datasources.findall('./datasource') if \
             self._datasources is not None else []
         datasource_names = list(dsxml.get('name') for dsxml in sub_datasources)
         return datasource_names
-
 
     def get_names_of_columns_per_datasource(self):
         names_per_ds = {}
@@ -71,23 +68,20 @@ class Dashboard(object):
                     names_per_ds[ds] = list(cl.column_name for cl in ds_dep.columns)
         return names_per_ds
 
-
     @property
     def dependent_on_datasources(self):
-        """List of data source names on which the worksheet is dependent.
+        """List of data source names on which the dashboard is dependent.
             :rtype: list()
         """
         return self._dependent_on_datasources
 
-
     @property
     def datasources_dependent_on_columns(self, ):
-        """Dictionary of data source names on which the worksheet is dependent together with columns of the data sources.
+        """Dictionary of data source names on which the dashboard XML is dependent together with columns of the data sources.
            keys: data source names.
            values: list of column names
         """
         return self._datasources_dependent_on_columns
-
 
     @property
     def datasources(self):
@@ -98,25 +92,10 @@ class Dashboard(object):
     def datasource_dependencies(self):
         return self._datasource_dependencies
 
-
     @property
     def zones_xml(self):
         return self._zones_xml
 
-    @zones_xml.setter
-    def zones_xml(self,value):
-        self._zones_xml = value
-
     @property
     def zones_with_param(self):
         return self._zones_with_param
-
-    # TODO: remove the follwoing function
-    # def modify_zones(self, datasource_name, field_names_to_be_changed_dict):
-    #
-    #     for zone in self.zones.findall('.//zone/*/zone[@param]'):
-    #         if datasource_name in zone.get('param'):
-    #             for field in field_names_to_be_changed_dict.keys():
-    #                 if field in zone.get('param'):
-    #                     new_param = field_names_to_be_changed_dict[field]
-    #                     zone.set('param', new_param)
