ce1952379f53a8db3378cbe179c5ae7d12fdb292
cedric deymier <cedric.deymier@wartsila.com>
2021-03-25 16:03:08 +0100

    modified the way we get zone elements in order to catch them all (pokemon...), even outside of zones tags as I identified zone with param elements on other areas

--- tableaudocumentapi/dashboard.py
+++ tableaudocumentapi/dashboard.py
@@ -41,9 +41,8 @@ class Dashboard(object):
 
         self._dependent_on_datasources = self.get_names_of_dependency_datasources()  # list of names
         self._datasources_dependent_on_columns = self.get_names_of_columns_per_datasource()
-        self._zones_xml = self._dashboardxmlelement.findall('./zones//zone[@param]')
+        self._zones_xml = self._dashboardxmlelement.findall('.//zone[@param]')
         self._zones_with_param = list(map(ZoneParam, self._zones_xml)) if self._zones_xml else []
-
 
 
     @property
